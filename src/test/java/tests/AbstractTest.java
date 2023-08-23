3b47d3e4e58d4bb02ad650a74ff0faa233257bf8
dspakhomov <vitaprox@mail.ru>
2023-08-23 14:34:02 +0300

    update

--- src/test/java/tests/AbstractTest.java
+++ src/test/java/tests/AbstractTest.java
@@ -19,34 +19,33 @@ public class AbstractTest {
 
     @BeforeSuite(alwaysRun = true)
     public void configOptions() {
-        Configuration.headless = true;
         Configuration.timeout = 20000;
         //Configuration.browserSize = "1600x940";
         //Configuration.holdBrowserOpen = true;
         Configuration.pageLoadTimeout = 60000;
         //Configuration.browserSize = "1600x920";
         Configuration.browser = "chrome";
-        var options = new ChromeOptions();
-        options.addArguments("–no-sandbox");
-        options.addArguments("–disable-dev-shm-usage");
+//        var options = new ChromeOptions();
+//        options.addArguments("–no-sandbox");
+//        options.addArguments("–disable-dev-shm-usage");
 //        Configuration.browserCapabilities.setCapability("goog:chromeOptions", Map.of( "args",
 //                List.of("–no-sandbox", "–disable-dev-shm-usage", "–remote-debugging-port=9222") ));
     }
 
-    // @BeforeClass(alwaysRun = true)
-    // public static void setUp() {
-    //     //Url удалённого веб драйвера
-    //     Configuration.remote = "http://localhost:4444/wd/hub";
-    //     //Размер окна браузера
-    //     //Создаём объект класса DesiredCapabilities, используется как настройка  вашей конфигурации с помощью пары ключ-значение
-    //     DesiredCapabilities capabilities = new DesiredCapabilities();
-    //     //Включить поддержку отображения экрана браузера во время выполнения теста
-    //     capabilities.setCapability("enableVNC", true);
-    //     //Включение записи видео в процессе выполнения тестов
-    //     capabilities.setCapability("enableVideo", true);
-    //     //Переопределяем Browser capabilities
-    //     Configuration.browserCapabilities = capabilities;
-    // }
+     @BeforeClass(alwaysRun = true)
+     public static void setUp() {
+         //Url удалённого веб драйвера
+         Configuration.remote = "http://localhost:4444/wd/hub";
+         //Размер окна браузера
+         //Создаём объект класса DesiredCapabilities, используется как настройка  вашей конфигурации с помощью пары ключ-значение
+         DesiredCapabilities capabilities = new DesiredCapabilities();
+         //Включить поддержку отображения экрана браузера во время выполнения теста
+         capabilities.setCapability("enableVNC", true);
+         //Включение записи видео в процессе выполнения тестов
+         capabilities.setCapability("enableVideo", true);
+         //Переопределяем Browser capabilities
+         Configuration.browserCapabilities = capabilities;
+     }
 
     @BeforeMethod(alwaysRun = true)
     public void before(){
